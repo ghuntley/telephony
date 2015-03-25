5e400ec365cd4fc9557bb9ca607fd3030ac7ed55
Geoffrey Huntley <ghuntley@ghuntley.com>
2015-03-25 14:23:12 +1100

    added page -> viewmodel bindings

--- samples/TelephonySampleApp.Core/Pages/HomePage.xaml.cs
+++ samples/TelephonySampleApp.Core/Pages/HomePage.xaml.cs
@@ -17,10 +17,12 @@ namespace TelephonySampleApp.Core.Pages
         {
             InitializeComponent();
 
-            //            this.Bind(ViewModel, vm => vm.Username, v => v.Username.Text);
-            //            this.Bind(ViewModel, vm => vm.Password, v => v.Password.Text);
-            //
-            //            this.BindCommand(ViewModel, vm => vm.Login, v => v.Login);
+            this.Bind(ViewModel, vm => vm.Recipient, v => v.Recipient.Text);
+            
+            this.BindCommand(ViewModel, vm => vm.ComposeEmail, v => v.ComposeEmail);
+            this.BindCommand(ViewModel, vm => vm.ComposeSMS, v => v.ComposeSMS);
+            this.BindCommand(ViewModel, vm => vm.MakePhoneCall, v => v.MakePhoneCall);
+            this.BindCommand(ViewModel, vm => vm.MakeVideoCall, v => v.MakeVideoCall);
         }
 
         public HomeViewModel ViewModel
